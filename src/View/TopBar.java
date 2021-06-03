4eb95c6060043fe38fd86f5d3a2c23c3238b5f76
or-fadlon <or5690@gmail.com>
2021-06-03 20:45:15 +0300

    top bar controller added

---
+++ src/View/TopBar.java
@@ -0,0 +1,27 @@
+package View;
+
+import javafx.event.ActionEvent;
+
+public class TopBar {
+    public void newOne(ActionEvent actionEvent) {
+    }
+
+    public void save(ActionEvent actionEvent) {
+    }
+
+    public void load(ActionEvent actionEvent) {
+    }
+
+    public void properties(ActionEvent actionEvent) {
+    }
+
+    public void exit(ActionEvent actionEvent) {
+        System.exit(5);
+    }
+
+    public void help(ActionEvent actionEvent) {
+    }
+
+    public void about(ActionEvent actionEvent) {
+    }
+}
